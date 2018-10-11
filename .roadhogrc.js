337c48a8802e9f731d28e430545db5b188081499
mrliang <619727242@qq.com>
2018-10-11 17:33:14 +0800

    2

---
+++ .roadhogrc.js
@@ -0,0 +1,34 @@
+export default {
+  entry: "src/index.js",
+  env: {
+    development: {
+      extraBabelPlugins: [
+        "dva-hmr",
+        "transform-runtime",
+        ['import', {'libraryName': 'antd', 'libraryDirectory': 'lib', 'style': true}]
+      ]
+    },
+    production: {
+      extraBabelPlugins: [
+        "transform-runtime",
+        ['import', {'libraryName': 'antd', 'libraryDirectory': 'lib', 'style': true}]
+      ]
+    },
+  },
+  sass:{},
+  proxy: {
+    "/api": {
+      "target": "http://192.168.31.47:3001",//
+      "changeOrigin": true,
+    },
+    "/admin": {
+      "target": "http://192.168.31.47:3001",
+      "changeOrigin": true,
+    },
+    "/static": {
+      "target": "http://192.168.31.47:3001",
+      "changeOrigin": true,
+    },
+  },
+  "disableCSSModules": true
+}
